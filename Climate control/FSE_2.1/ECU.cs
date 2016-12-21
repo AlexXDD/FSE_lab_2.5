7b9cd88eacd37f64af08b991b76d18a92f365128
Alex <bender.rulit@gmail.com>
2016-12-21 18:47:51 +0200

    Adding project to repository

---
+++ Climate control/FSE_2.1/ECU.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSE_2._1
+{
+    public class ECU
+    {
+        private flowDirection airFlow;
+        private Sensor sensor;
+
+        public ECU()
+        {
+            airFlow = flowDirection.automatic;
+            sensor = new Sensor();
+        }
+
+        public flowDirection AirDirection
+        {
+            get
+            {
+                return airFlow;
+            }
+
+            set
+            {
+                airFlow = value;
+            }
+        }
+
+        public int Temperature
+        {
+            get
+            {
+                return sensor.InteriorTemperature;
+            }
+
+            set
+            {
+                sensor.InteriorTemperature = value;
+            }
+        }
+
+        public enum flowDirection {automatic, legs, face,
+            face_legs, windshield, windshield_legs  }
+    }
+}
---
+++ Climate control/FSE_2.1/Sensor.cs
@@ -0,0 +1,33 @@
+﻿namespace FSE_2._1
+{
+    public class Sensor
+    {
+        private int exteriorTemp, interiorTemp;
+
+        public int ExteriorTemperature
+        {
+            get
+            {
+               return exteriorTemp;
+            }
+        }
+
+        public int InteriorTemperature
+        {
+            get
+            {
+                return interiorTemp;
+            }
+        }
+
+        public void IncreaseTemperature()
+        {
+            interiorTemp++;
+        }
+
+        public void DecreaseTemperature()
+        {
+            interiorTemp--;
+        }
+    }
+}
